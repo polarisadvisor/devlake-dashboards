2ae0d5588a8abe8048b60ff151fc203941dea650
Krishna Kumar <kkumar@exathink.com>
2025-02-08 18:03:30 -0600

    POD-12:Add test package.

---
+++ dashboard_testing/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC 2016-2015-${today.year} All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
